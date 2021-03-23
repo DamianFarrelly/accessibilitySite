503646a9c94441ff85966635156423a97650473e
Damian Farrelly <damian.farrelly@atos.net>
2021-03-23 15:46:27 +0000

    Footer & Validation
    Added updated footer and fields with some validation

--- account/anything.php
+++ account/anything.php
@@ -2,7 +2,8 @@
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 $link = mysqli_connect("localhost", "root", "", "accountcreation");
- 
+
+
 // Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
@@ -17,9 +18,15 @@ $enteredPassword = $_POST['enteredPassword'];
 
 $sql = "INSERT INTO users (fname, lname, username, password) VALUES ('$fname','$lname','$userUsername','$enteredPassword')";
 if(mysqli_query($link, $sql)){
-    echo "Records inserted successfully.";
+    $message = "Account created successfully, redirecting to login page...";
+    echo "<script type='text/javascript'>alert('$message');</script>";
+    echo "<script type='text/javascript'>window.location.href='login.html'</script>";
+
 } else{
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+    $message = "The username is taken";
+    echo "<script type='text/javascript'>alert('$message');</script>";
+    echo "<script type='text/javascript'>window.location.href='creation.html'</script>";
+
 }
  
 // Close connection
